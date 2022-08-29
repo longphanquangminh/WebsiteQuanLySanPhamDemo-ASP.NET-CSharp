f5203673c26998ee59cca1a030e4f0d7317aa597
Phan Quang Minh Long <lordaizensousuke2001@gmail.com>
2022-08-29 20:31:05 +0700

    up

--- WebsiteQuanLySanPham/Controllers/CatalogController.cs
+++ WebsiteQuanLySanPham/Controllers/CatalogController.cs
@@ -18,5 +18,56 @@ namespace WebsiteQuanLySanPham.Controllers
             return View(dsCatalog);
 
         }
+
+        public ActionResult Details(int id)
+        {
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext();
+            Catalog cate = context.Catalogs.FirstOrDefault(x => x.Id == id);
+            return View(cate);
+        }
+
+        public ActionResult SanPhams(int cataLogId)
+        {
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext();
+            List<Product> dsProduct = context.Products.Where(x => x.CatalogId == cataLogId).ToList();
+            return View(dsProduct);
+        }
+
+        public ActionResult Create()
+        {
+            if (Request.Form.Count > 0)
+            {
+                string catalogCode = Request.Form["CatalogCode"];
+                string catalogName = Request.Form["CatalogName"];
+            
+
+                QuanLySanPhamDataContext context = new QuanLySanPhamDataContext();
+
+                Catalog catalog = new Catalog();
+                List<Catalog> dsCatalog = context.Catalogs.ToList();
+                catalog.Id = dsCatalog.Count+1;
+                catalog.CatalogCode = catalogCode;
+                catalog.CatalogName = catalogName;
+                context.Catalogs.InsertOnSubmit(catalog);
+                context.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext();
+            Catalog catalog = context.Catalogs.FirstOrDefault(x => x.Id == id);
+            if (Request.Form.Count == 0)
+            {
+                return View(catalog);
+            }
+            string catalogCode = Request.Form["CatalogCode"];
+            string catalogName = Request.Form["catalogName"];
+            catalog.CatalogCode = catalogCode;
+            catalog.CatalogName = catalogName;
+            context.SubmitChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- WebsiteQuanLySanPham/Controllers/ProductController.cs
+++ WebsiteQuanLySanPham/Controllers/ProductController.cs
@@ -13,9 +13,80 @@ namespace WebsiteQuanLySanPham.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext();
+            /*QuanLySanPhamDataContext context = new QuanLySanPhamDataContext();
             List<Product> dsProduct = context.Products.ToList();
+            return View(dsProduct);*/
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext();
+            List<Product> dsProduct = null;
+            if (Request.QueryString.Count == 0)
+            {
+                dsProduct = context.Products.ToList();
+            }
+            else
+            {
+                double min = double.Parse(Request.QueryString["txtMin"]);
+                double max = double.Parse(Request.QueryString["txtMax"]);
+                dsProduct = context.Products
+                    .Where(x => x.UnitPrice >= min && x.UnitPrice <= max)
+                    .ToList();
+            }
             return View(dsProduct);
+        }
+        public ActionResult Details(int id)
+        {
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext();
+            Product p = context.Products
+                               .FirstOrDefault(x => x.Id == id);
+            return View(p);
+        }
+        public ActionResult Create()
+        {
+            if (Request.Form.Count > 0)
+            {
+                QuanLySanPhamDataContext context = new QuanLySanPhamDataContext();
+                Product p = new Product();
+                List<Product> dsProduct = context.Products.ToList();
+                p.Id = dsProduct.Count + 1;
+                p.CatalogId = int.Parse(Request.Form["CatalogId"]);
+                p.ProductCode = Request.Form["ProductCode"];
+                p.ProductName = Request.Form["ProductName"];
+                p.UnitPrice = double.Parse(Request.Form["UnitPrice"]);
+                HttpPostedFileBase file = Request.Files["Picture"];
+                if (file != null)
+                {
+                    string serverPath = HttpContext.Server.MapPath("~/Hinh");
+                    string filePath = serverPath + "/" + file.FileName;
+                    file.SaveAs(filePath);
+                    p.Picture = file.FileName;
+                }
+                context.Products.InsertOnSubmit(p);
+                context.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext();
+            Product p = context.Products.FirstOrDefault(x => x.Id == id);
+            if (Request.Form.Count == 0)
+            {
+                return View(p);
+            }
+            p.CatalogId = int.Parse(Request.Form["CatalogId"]);
+            p.ProductCode = Request.Form["ProductCode"];
+            p.ProductName = Request.Form["ProductName"];
+            p.UnitPrice = double.Parse(Request.Form["UnitPrice"]);
+            HttpPostedFileBase file = Request.Files["Picture"];
+            if (file != null && file.FileName != "")
+            {
+                string serverPath = HttpContext.Server.MapPath("~/Hinh");
+                string filePath = serverPath + "/" + file.FileName;
+                file.SaveAs(filePath);
+                p.Picture = file.FileName;
+            }
+            context.SubmitChanges();
+            return RedirectToAction("Index");
         }
     }
 }
