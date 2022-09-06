d9f161218f819adcf9c9d54b3dce26e3f68c4682
Phan Quang Minh Long <lordaizensousuke2001@gmail.com>
2022-09-06 20:27:17 +0700

    up sql command

---
+++ SQL_Query_files_for_database/SQLQueryDotNet2.sql
@@ -0,0 +1,47 @@
+﻿USE [QuanLySanPham]
+GO
+
+UPDATE [dbo].[Catalog]
+   SET 
+      [CatalogName] = N'Điện Thoại'
+ WHERE id=1
+GO
+
+UPDATE [dbo].[Catalog]
+   SET 
+      [CatalogName] = N'Máy Tính'
+ WHERE id=2
+GO
+
+UPDATE [dbo].[Catalog]
+   SET 
+      [CatalogName] = N'Thời Trang'
+ WHERE id=3
+GO
+
+UPDATE [dbo].[Catalog]
+   SET 
+      [CatalogName] = N'Gia Dụng'
+ WHERE id=4
+GO
+
+USE [QuanLySanPham]
+GO
+
+-- 
+
+DELETE FROM [dbo].[Catalog]
+      WHERE id=0
+GO
+
+--
+
+DELETE FROM [dbo].[Product]
+      WHERE id=0
+GO
+
+--
+SET IDENTITY_INSERT [dbo].[Catalog] OFF
+SET IDENTITY_INSERT [dbo].[Product] OFF
+SET IDENTITY_INSERT [dbo].[Catalog] ON
+SET IDENTITY_INSERT [dbo].[Product] ON
---
+++ SQL_Query_files_for_database/SQLQueryDotNet3.sql
@@ -0,0 +1,53 @@
+﻿USE [QuanLySanPham]
+GO
+
+INSERT INTO [dbo].[Catalog]
+           (
+		   [Id]
+           ,
+		   [CatalogCode]
+           ,[CatalogName])
+     VALUES
+           (
+		   1,
+		   'DM01',N'Điện Thoại'),
+           (
+		   2,
+		   'DM02',N'Máy Tính'),
+           (
+		   3,
+		   'DM03',N'Thời Trang'),
+           (
+		   4,
+		   'DM04',N'Gia Dụng')
+GO
+
+
+
+
+INSERT INTO [dbo].[Product]
+           (
+		   [Id]
+           ,
+		   [CatalogId]
+           ,[ProductCode]
+		   ,[ProductName]
+		   ,[Picture]
+		   ,[UnitPrice])
+     VALUES
+           (
+		   1,
+		   1,'PRO01','SamSung J7 Prime','PRO01.PNG',10000000),
+           (
+		   2,
+		   1,'PRO02','Iphone X','PRO02.PNG',30000000),
+           (
+		   3,
+		   2,'PRO03','Dell Inspiron N3552','PRO03.PNG',12000000),
+           (
+		   4,
+		   2,'PRO04','Dell Inspiron N3467','PRO04.PNG',13000000),
+           (
+		   5,
+		   2,'PRO05','Acer AS A315-31-C8GB','PRO05.PNG',9000000)
+GO
